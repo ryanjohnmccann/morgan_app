27c58310ca9032f5fd1a397ba6a5448eea7a4ad7
Ryan McCann <ryanmccann@ryans-mbp-4.lan>
2020-05-10 10:28:53 -0400

    Added BrowserRouter

--- vsc_scripts/morgan_app_ui/src/index.js
+++ vsc_scripts/morgan_app_ui/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './Main.js';
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
-  <React.StrictMode>
+
+  <BrowserRouter>
     <Main/>
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
+  
 );
