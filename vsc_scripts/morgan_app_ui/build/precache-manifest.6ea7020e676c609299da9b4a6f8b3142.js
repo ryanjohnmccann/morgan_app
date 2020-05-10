8d575af5066fe9348693556e75ec98f193138fea
Ryan McCann <ryanmccann@ryans-mbp-4.lan>
2020-05-10 11:09:38 -0400

    Optimized production build

--- vsc_scripts/morgan_app_ui/build/precache-manifest.6ea7020e676c609299da9b4a6f8b3142.js
+++ vsc_scripts/morgan_app_ui/build/precache-manifest.6ea7020e676c609299da9b4a6f8b3142.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d73b1b467f84faea55592aef7046ae23",
+    "revision": "15a4efb70a69f577e26b6dd7352b5cc7",
     "url": "/index.html"
   },
   {
-    "revision": "ef23f5582e190b4e0d21",
-    "url": "/static/js/2.d7e5c475.chunk.js"
+    "revision": "803adbcc06995f003f6c",
+    "url": "/static/js/2.25230dfe.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/static/js/2.d7e5c475.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.25230dfe.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "594ed3d4989814914dc7",
-    "url": "/static/js/main.1dc2b59b.chunk.js"
+    "revision": "33df58b68a010f2a193c",
+    "url": "/static/js/main.ed08c53f.chunk.js"
   },
   {
     "revision": "a30434575de05f0b725c",
